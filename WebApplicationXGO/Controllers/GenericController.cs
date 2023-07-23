c03d62fd362a3ca5f90598296eb60c5ab3890cb5
sami-debug <84708497+sami-debug@users.noreply.github.com>
2023-07-23 17:19:15 +0200

    add attribute

--- WebApplicationXGO/Controllers/GenericController.cs
+++ WebApplicationXGO/Controllers/GenericController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using System.Text.Json;
