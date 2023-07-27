80d9b0417f9d3f65e01f1c8fbfd4e61a7471d1c0
Sami BOUHAJJA <bouhajja.s@sfeir.com>
2023-07-28 00:24:19 +0200

    fix connection string issue

--- WebApplicationXGO/Program.cs
+++ WebApplicationXGO/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 using XGORepository.Interfaces.RepositoriesInterfaces;
@@ -17,10 +18,19 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<XGODbContext>(options =>
+#if DEBUG
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"), builder =>
     {
         builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
-    }));
+    })
+#else
+options.UseSqlServer(Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING"), builder =>
+        {
+            builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        })
+
+#endif
+    );
 
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddTransient<ISubCategoryRepository, SubCategoryRepository>();
