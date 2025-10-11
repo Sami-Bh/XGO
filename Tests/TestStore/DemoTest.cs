5baf680236cccd199cbc4ed984943345d723c5ee
Sami BOUHAJJA <bouhajja.s@sfeir.com>
2025-10-11 17:27:13 +0200

    init tests setup

---
+++ Tests/TestStore/DemoTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestStore
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void DemoTestRun()
+        {
+            Assert.True(1 > 0);
+        }
+    }
+}
