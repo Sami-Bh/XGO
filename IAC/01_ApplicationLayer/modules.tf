30ea8a32d3159c0368f54a7fa6cf6cb33ef88268
Sami BOUHAJJA <bouhajja.s@sfeir.com>
2025-10-15 17:39:42 +0200

    start create terraform for git setup

--- IAC/01_ApplicationLayer/modules.tf
+++ IAC/01_ApplicationLayer/modules.tf
@@ -1,9 +1,9 @@
 module "unique_pictures_storage_accountName" {
-  source       = "./modules/unique_name_or_id_generator"
+  source       = "../modules/unique_name_or_id_generator"
   input_string = var.picturesStorageAccountName
 }
 
 module "unique_pictures_computer_vision_name" {
-  source       = "./modules/unique_name_or_id_generator"
+  source       = "../modules/unique_name_or_id_generator"
   input_string = var.picturesComputerVisionName
 }
