e9ebe21fc1bb934b948bb8aa99c0b85e5e0f3cbc
Sami BOUHAJJA <bouhajja.s@sfeir.com>
2025-10-26 23:09:10 +0100

    Add budget alert Terraform config for GithubSetupLayer
    Introduces a new Terraform file to define Azure budget alerts and action groups in the GithubSetupLayer. Also updates .gitignore to exclude emails.tf from version control.

---
+++ IAC/00_GithubSetupLayer/budget _alert.tf
@@ -0,0 +1,58 @@
+resource "azurerm_monitor_action_group" "example" {
+  name                = "budget monitor action"
+  resource_group_name = azurerm_resource_group.rg-xgo.name
+  short_name          = "budget monitor action"
+}
+
+resource "azurerm_consumption_budget_resource_group" "example" {
+  name              = "total budget"
+  resource_group_id = azurerm_resource_group.example.id
+
+  amount     = 2
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2025-10-26T00:00:00Z"
+    end_date   = "2027-10-26T00:00:00Z"
+  }
+
+  filter {
+    dimension {
+      name = "ResourceId"
+      values = [
+        azurerm_monitor_action_group.example.id,
+      ]
+    }
+  }
+
+  notification {
+    enabled        = true
+    threshold      = 90.0
+    operator       = "EqualTo"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      local.email_sami,
+      local.email_rim,
+    ]
+
+    contact_groups = [
+      azurerm_monitor_action_group.example.id,
+    ]
+
+    contact_roles = [
+      "Owner",
+    ]
+  }
+
+  notification {
+    enabled   = true
+    threshold = 100.0
+    operator  = "GreaterThan"
+
+    contact_emails = [
+      local.email_sami,
+      local.email_rim,
+    ]
+  }
+}
