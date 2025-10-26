c9ce29fa6cdba24ef3e436e46bdaa0be6a1bf7ae
Sami BOUHAJJA <bouhajja.s@sfeir.com>
2025-10-26 23:18:37 +0100

    Update Terraform workflow and budget alert config
    Add '-upgrade' flag to 'terraform init' in the GitHub Actions workflow to ensure provider and module updates. Adjust budget alert Terraform: fix resource group reference, update short_name, and modify budget time period dates.

--- IAC/00_GithubSetupLayer/budget _alert.tf
+++ IAC/00_GithubSetupLayer/budget _alert.tf
@@ -1,19 +1,19 @@
 resource "azurerm_monitor_action_group" "example" {
   name                = "budget monitor action"
   resource_group_name = azurerm_resource_group.rg-xgo.name
-  short_name          = "budget monitor action"
+  short_name          = "mon action"
 }
 
 resource "azurerm_consumption_budget_resource_group" "example" {
   name              = "total budget"
-  resource_group_id = azurerm_resource_group.example.id
+  resource_group_id = azurerm_resource_group.rg-xgo.id
 
   amount     = 2
   time_grain = "Monthly"
 
   time_period {
-    start_date = "2025-10-26T00:00:00Z"
-    end_date   = "2027-10-26T00:00:00Z"
+    start_date = "2025-10-01T00:00:00Z"
+    end_date   = "2027-10-01T00:00:00Z"
   }
 
   filter {
