43533a4d032e36633f2ce9eeb08df44fbd732ea5
Sami BOUHAJJA <bouhajja.s@sfeir.com>
2025-12-29 17:10:56 +0100

    Add Azure AD authentication to Store and Storage APIs
    Integrated Microsoft.Identity.Web v4.2.0 and configured JWT Bearer authentication in both XGO.Store.Api and XGO.Storage.Api. Updated Terraform web app resources to set AzureAd app settings. Cleaned up appsettings.Development.json files and updated .gitignore to exclude them. Also updated package dependencies to support the new authentication setup and added custom authorization policy to the API Gateway routes.

--- XGO.Storage/XGO.Storage.Api/Program.cs
+++ XGO.Storage/XGO.Storage.Api/Program.cs
@@ -1,6 +1,8 @@
 
 using BuildingBlocks.CQRS.Generic.Queries;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
 using XGO.Storage.Api.Storage.Application.MappingProfiles;
 using XGO.Storage.Api.Storage.Domain;
 using XGO.Storage.Api.Storage.Persistence;
@@ -13,6 +15,8 @@ namespace XGO.Storage.api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddMicrosoftIdentityWebApi(builder.Configuration);
             builder.Services.AddDbContext<XgoStorageDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
