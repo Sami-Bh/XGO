a19a0a677685a62b427e7e04876566e0f3743ab0
Sami BOUHAJJA <bouhajja.s@sfeir.com>
2026-02-01 16:14:08 +0100

    Add IsExpiracyAcknowledged column to StoredItems
    Introduces a new boolean column 'IsExpiracyAcknowledged' to the StoredItems table via a migration. Updates the model snapshot to reflect this schema change.

---
+++ XGO.Storage/XGO.Storage.Api/Storage.Persistence/Migrations/20260201151345_addAcknowledgedColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XGO.Storage.Api.Storage.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAcknowledgedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsExpiracyAcknowledged",
+                table: "StoredItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsExpiracyAcknowledged",
+                table: "StoredItems");
+        }
+    }
+}
